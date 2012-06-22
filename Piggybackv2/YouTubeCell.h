e4a0f80ee696e8236aca2f8a3292b8d0ef883ae7
Kimberly Hsiao <kimberly.hsiao@gmail.com>
2012-06-22 00:54:39 -0400

    heart and todo buttons

--- Piggybackv2/YouTubeCell.h
+++ Piggybackv2/YouTubeCell.h
@@ -14,5 +14,10 @@
 @property (nonatomic, weak) IBOutlet UILabel* favoritedBy;
 @property (nonatomic, weak) IBOutlet UIImageView* profilePic;
 @property (nonatomic, weak) IBOutlet UILabel* date;
+@property (nonatomic, weak) IBOutlet UIButton* heart;
+@property (nonatomic, weak) IBOutlet UIButton* todo;
+
+- (IBAction)heart:(id)sender;
+- (IBAction)todo:(id)sender;
 
 @end
--- Piggybackv2/YouTubeCell.m
+++ Piggybackv2/YouTubeCell.m
@@ -14,5 +14,17 @@
 @synthesize favoritedBy = _favoritedBy;
 @synthesize profilePic = _profilePic;
 @synthesize date = _date;
+@synthesize heart = _heart;
+@synthesize todo = _todo;
+
+- (IBAction)heart:(id)sender {
+    [self.heart setImage:[UIImage imageNamed:@"heart-pressed-button"] forState:UIControlStateNormal];
+}
+
+- (IBAction)todo:(id)sender {
+    [self.todo setImage:[UIImage imageNamed:@"todo-added-button"] forState:UIControlStateNormal];
+    
+}
 
 @end
+
