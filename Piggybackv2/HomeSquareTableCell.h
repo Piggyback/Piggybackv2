da59f49729b97b9980772853dd4d93ec874ed281
Kimberly Hsiao <kimberly.hsiao@gmail.com>
2012-07-24 13:24:56 -0400

    updated news feed- new cell layout

---
+++ Piggybackv2/HomeSquareTableCell.h
@@ -0,0 +1,13 @@
+//
+//  HomeSquareTableCell.h
+//  Piggybackv2
+//
+//  Created by Kimberly Hsiao on 7/24/12.
+//  Copyright (c) 2012 Calimucho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeSquareTableCell : UITableViewCell
+
+@end
